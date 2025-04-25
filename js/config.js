844df3feac61c574a8f3242cbc79ea16ddb372b6
Alexander Green <alexander.green@logius.nl>
2025-04-25 11:53:55 +0200

    Beheer: scrap superfluous script and use `subtitle` related to https://github.com/Logius-standaarden/ReSpec-template/issues/41

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ var respecConfig = {
   shortName: "opensource",
   publishDate: "2023-01-31",
   publishVersion: "0.0.1",
-  title: "BOMOSS open source",
+  subtitle: "BOMOS voor open source",
   // previousPublishVersion: "(none)",
   content: {"ch01": "informative", "ch02": "", "mermaid": ""},
   editors:
